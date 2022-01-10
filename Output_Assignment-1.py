344e7e883341a99fe867743654dbd40aa672ca6e
Abhinavagg2002 <97080541+Abhinavagg2002@users.noreply.github.com>
2022-01-10 16:08:21 +0530

    Add files via upload

---
+++ Output_Assignment-1.py
@@ -0,0 +1,23 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: E:\Abhinav\PEC\Computing\Assignment-1.py ==============
+Enter the first number 10
+Enter the second number 20
+Enter the third number 30
+The average pf these numbers is  20.0
+Enter your income 100000
+Enter number of dependent 2
+The value of deducted amount is  6000
+The value of tax is 16800.0
+The value of taxable income is  84000
+Enter your SID 21103030
+Enter your name Abhinav Aggarwal
+Enter your gender {Use 'M' for 'Male' and 'F' for 'Female' M
+Enter your course name CSE
+Enter your CGPA 8.6
+[21103030, 'Abhinav Aggarwal', 'M', 'CSE', 8.6]
+Enter the marks of five students: 30 35 28 40 25
+[25, 28, 30, 35, 40]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Purple']
