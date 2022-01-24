956893cc1e8b26f5a079b6c6c00d9436f234e21b
Abhinavagg2002 <97080541+Abhinavagg2002@users.noreply.github.com>
2022-01-24 15:55:46 +0530

    Add files via upload

---
+++ Output_Assign-2_21103030_Abhinav.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: E:/Abhinav/PEC/Computing/Assignment/Assignment-2/Assign-2_21103030_Abhinav.py
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+Enter your name: Abhinav Aggarwal
+Enter your SID: 21103030
+Enter your department: CSE
+Enter your CGPA: 10
+Hey,Abhinav Aggarwal Here!
+My SID is 21103030
+I am from CSE department and my CGPA is 10
+Part a:  8
+Part b:  58
+Part c:  50
+First part of Part d: 224
+Second part of Part d: 40
+First part of Part e:  14
+Second part of Part e  0
+Enter the first number: 100
+Enter the second number: 50
+Enter the third number: 75
+ The first number is greatest
+Enter a statement: Hi everyone my name is Abhinav Aggarwal
+Yes
+Enter the length of first side of triangle: 10
+Enter the length of second side of triangle: 20
+Enter the length of third side of triangle: 40
+NO
