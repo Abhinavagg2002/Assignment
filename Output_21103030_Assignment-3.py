b34c8cdef0c9199257aaac767faf09e536916ed3
Abhinavagg2002 <97080541+Abhinavagg2002@users.noreply.github.com>
2022-02-14 23:56:41 +0530

    Add files via upload

---
+++ Output_21103030_Assignment-3.py
@@ -0,0 +1,75 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: E:\Abhinav\PEC\Computing\Assignment\21103030_Assignment-3_Computing.py
+Question 1
+Enter a string: My name is Abhinav Aggarwal
+Total occurances
+my-1
+name-1
+is-1
+abhinav-1
+aggarwal-1
+Question-2
+Please enter Day- 31
+Please enter Month- 12
+Please enter Year- 2021
+Next Date is: 1/1/2022
+Question-3
+Please enter Y to enter data- Y
+Plz give an integer input- 87
+Please enter Y to enter data- N
+[87]
+[(87, 7569)]
+Question-4
+Enter a grade point: 9
+Your Grade is A and excellent Performance
+Question-5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+Question 6
+Please enter your SID: 21103030
+Please enter your name: Abhinav Aggarwal
+If you want to enter more details press Y/N: N
+Part a
+21103030 is Abhinav Aggarwal
+
+Part b
+value sorted dictionary is ['Abhinav Aggarwal']
+
+Part c
+Keys sorted dictionary is [21103030]
+
+Part D
+Please enter the student's SID whose detail you need- 21103041
+This SID is not present in the given Data
+
+Question 7
+Total elements of Fibonacci sequence that you want: 9
+Fibonnaci sequence with 9 terms
+0
+1
+1
+2
+3
+5
+8
+13
+21
+Average of total 9 terms of sequence is 6.0
+END
+Question 8
+Part-a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part-b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part-c
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part-d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3 {10, 7}
