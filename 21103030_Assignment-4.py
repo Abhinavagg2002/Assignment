2b1cc5c88d985d3d45dd68e6ec299bba676dc36d
Abhinavagg2002 <97080541+Abhinavagg2002@users.noreply.github.com>
2022-02-25 23:18:16 +0530

    Add files via upload

---
+++ 21103030_Assignment-4.py
@@ -0,0 +1,59 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================ RESTART: Shell ================================
+
+============================================== RESTART: E:/Abhinav/PEC/Computing/Assignment/21103030_Assignment-4.py ==============================================
+Question-1
+Enter the number of discs: 4
+Move disc 1 from A to C
+Move disc 2 from A to B
+Move disc 1 from C to B
+Move disc 3 from A to C
+Move disc 1 from B to A
+Move disc 2 from B to C
+Move disc 1 from A to C
+Move disc 4 from A to B
+Move disc 1 from C to B
+Move disc 2 from C to A
+Move disc 1 from B to A
+Move disc 3 from C to B
+Move disc 1 from A to C
+Move disc 2 from A to B
+Move disc 1 from C to B
+Question-2
+Enter the number of rows: 5
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+     
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+Question-3
+Enter first number: 9
+Enter second number: 7
+The quotient is 1 and the remainder is 2
+False
+False
+True
+True
+[1, 2, 4, 5, 6]
+[1, 2, 4]
+{1, 2, 4}
+frozenset({1, 2, 4})
+4
+4
+Question-4
+Student's name is Abhinav
+Student's SID is 21103030
+Question-5
+70000
+Question-6
+Enter George's word: Cricket
+Enter Barbie's word: Football
+Friendship is Fake
